82917872851338de8e4dffa0cf523c43d52ba387
thsvo <155262940+thsvo@users.noreply.github.com>
2025-07-23 19:27:03 +0600

    added ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  apps: [
+    {
+      name: "next-app",
+      script: "npm",
+      args: "run start"
+    },
+    {
+      name: "backend-server",
+      script: "npm",
+      args: "run server"
+    }
+  ]
+};
